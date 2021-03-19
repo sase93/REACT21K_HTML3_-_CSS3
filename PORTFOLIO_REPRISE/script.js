3f3cd2d99c11e863b3cbf0236bcd4daa6a8cf8ce
Samu Rahkila <samu.rahkila@gmail.com>
2021-03-19 19:39:32 +0200

    Color and margin adjustments, beginning of other devices' support

---
+++ PORTFOLIO_REPRISE/script.js
@@ -0,0 +1,13 @@
+let header = document.querySelector(".logo_and_nav");
+
+const scrollHeader = () => {
+    if (document.body.scrollTop > 210 || document.documentElement.scrollTop > 210) {
+        header.style.background = "#eaf0f5";
+        header.style.padding = "1%";
+    } else {
+        header.style.background = "transparent";
+        header.style.padding = "1%";
+    }
+};
+
+window.addEventListener("scroll", scrollHeader);
