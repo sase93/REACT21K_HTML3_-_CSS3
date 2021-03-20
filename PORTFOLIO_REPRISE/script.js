a06b1e742cabf3291824fdf1cbec29c4af0d5ec4
Samu Rahkila <samu.rahkila@gmail.com>
2021-03-20 14:28:41 +0200

    Added favicon, Skills section icons, and back to top button

--- PORTFOLIO_REPRISE/script.js
+++ PORTFOLIO_REPRISE/script.js
@@ -1,3 +1,5 @@
+// ********** Header functions **********
+
 let header = document.querySelector(".logo_and_nav");
 let mobileLinks = document.querySelector(".menu-links");
 
@@ -20,4 +22,23 @@ function toggleMenu() {
     } else {
       x.style.display = "initial";
     }
+}
+
+// ********** Back to top button functions **********
+
+let topButton = document.getElementById("back-to-top");
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 250 || document.documentElement.scrollTop > 250) {
+    topButton.style.display = "block";
+  } else {
+    topButton.style.display = "none";
+  }
+}
+
+function returnTop() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
 }
